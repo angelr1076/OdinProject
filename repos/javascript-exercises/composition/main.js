722b201adfbd2378b0b691850fb43933884d034b
angelr1076 <angelr1076@gmail.com>
2022-11-03 15:36:56 -0400

    Updated factory functions

--- repos/javascript-exercises/composition/main.js
+++ repos/javascript-exercises/composition/main.js
@@ -6,10 +6,10 @@ const driver = state => ({
   drive: () => (state.position = state.position + state.speed),
 });
 
-const eater = state => ({
-  eat: () =>
-    console.log(state.name + ' is so hungry he can eat ' + state.eatObject),
-});
+// const eater = state => ({
+//   eat: () =>
+//     console.log(state.name + ' is so hungry he can eat ' + state.eatObject),
+// });
 
 barker({ name: 'karo' }).bark();
 // Woof, I am karo
@@ -23,7 +23,66 @@ const murderRobotDog = name => {
   };
   return Object.assign({}, barker(state), driver(state), eater(state));
 };
+
 const bruno = murderRobotDog('bruno');
 const eddie = murderRobotDog('eddie');
+console.log({ bruno, eddie });
 bruno.bark(); // "Woof, I am Bruno"
-eddie.eat();
+// eddie.eat();
+
+function swimmer({ name }) {
+  return {
+    swim: () => console.log(`${name} swam`),
+  };
+}
+
+function flyer({ name }) {
+  return {
+    fly: () => console.log(`${name} flew`),
+  };
+}
+
+function eater({ name }) {
+  return {
+    eat: () => console.log(`${name} ate voraciously`),
+  };
+}
+
+function swimmingMonsterCreator(name) {
+  const monster = { name };
+
+  return {
+    ...monster,
+    ...swimmer(monster),
+  };
+}
+
+function flyingMonsterCreator(name) {
+  const monster = { name };
+
+  return {
+    ...monster,
+    ...swimmer(monster),
+    ...flyer(monster),
+  };
+}
+
+function eatingMonsterCreator(name) {
+  const monster = { name };
+
+  return {
+    ...monster,
+    ...swimmer(monster),
+    ...flyer(monster),
+    ...eater(monster),
+  };
+}
+
+// const muriel = swimmer({ name: 'Eagle' });
+// muriel.swim();
+// const wiggly = swimmingMonsterCreator('Wiggly');
+// const booger = flyingMonsterCreator('Booger');
+const godzilla = eatingMonsterCreator('Godzilla');
+godzilla.swim();
+godzilla.fly();
+godzilla.eat();
